70c17cee4c3eb3336cf5c8a7ceee39af32730d63
Abderrahmane Laajili <alaajili@e1r13p6.1337.ma>
2023-08-01 02:42:54 +0100

    scale the canvas with the with of window

--- client-side/src/Game.tsx
+++ client-side/src/Game.tsx
@@ -13,12 +13,31 @@ interface GameProps extends SketchProps {
 }
 
 const game: Sketch<GameProps> = (p5) => {
-  let playerY: number;
+  let playerY: number = 250;
   let ball: Ball = {x: 500, y:300};
+  let width = 1000;
+  let height  = 600;
 
+  let scale = 1;
   
-  p5.setup = () => { 
-    p5.createCanvas(1000, 600);
+  const getWidth = () => {
+    if (p5.windowWidth <= 600) {
+      scale = 0.5;
+    }
+    else if (p5.windowWidth <= 800) {
+      scale = 0.6;
+    }
+    else if (p5.windowWidth <= 1000) {
+      scale = 0.8;
+    }
+    else {
+      scale = 1;
+    }
+  }
+
+  p5.setup = () => {
+    getWidth();
+    p5.createCanvas(width*scale, height*scale);
   }
 
   p5.updateWithProps = (props) => { 
@@ -30,19 +49,23 @@ const game: Sketch<GameProps> = (p5) => {
     }
   }
   
-
+  p5.windowResized = () => {
+    getWidth();
+    p5.resizeCanvas(width*scale, height*scale);
+  }
+  
 
   p5.draw = () => {
     
     p5.clear();
     p5.stroke(255);
     p5.strokeWeight(2);
-    p5.ellipse(500, 300, 15);
-    p5.line(500, 0, 500, 600);
+    p5.ellipse((width*scale)/2, (height*scale)/2, 15*scale);
+    p5.line((width*scale)/2, 0, (width*scale)/2, height*scale);
     
-    p5.rect(4 ,playerY, 8, 80, 50);
+    p5.rect(4*scale ,playerY*scale, 8*scale, 80*scale, 50);
     
-    // p5.ellipse(ball.x, ball.y, 15);
+    p5.ellipse(ball.x, ball.y, 15);
 
 
   }
